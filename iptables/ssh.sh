46a2ad09b37916458299cec740415cbda975aff8
Barney Hanlon <barney@shrikeh.net>
2014-06-26 15:25:08 +0100

    Add IPs to separate file, update iptables script with IPs for frontdoor v2

--- iptables/ssh.sh
+++ iptables/ssh.sh
@@ -3,7 +3,7 @@
 function iptables_ssh() {
   declare -a IPTABLES='/sbin/iptables'
 
-  local IPS_ACCEPT=('81.23.63.12' '141.0.34.138' '62.49.114.11' '88.208.220.171' '37.191.108.74' '78.109.163.185')
+  local IPS_ACCEPT=('184.72.232.234' '23.21.72.114' '81.23.63.12' '141.0.34.138' '62.49.114.11' '88.208.220.171' '37.191.108.74' '78.109.163.185')
 
   # Accept local connections
   ${IPTABLES} -A INPUT -p tcp --dport ssh -i lo -j ACCEPT
