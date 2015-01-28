b6bb87b35bdb860ad784e3f2dc4d7a077a50c73b
Barney Hanlon <bhanlon@inviqa.com>
2015-01-28 13:34:20 +0000

    Rename script to show this is for provisioning

--- deployment/create_sudo_provisioners_group.sh
+++ deployment/create_sudo_provisioners_group.sh
@@ -4,10 +4,10 @@ set -e
 
 function create_deploy_sudo() {
 
-  local SUDOERS_FILE='/etc/sudoers.d/deploy';
+  local SUDOERS_FILE='/etc/sudoers.d/provisioners';
   local TMP_DIR=$(mktemp -dt "$(basename $0).XXXXXXXXXX");
   local SUDO_GID='2000';
-  local TMP_FILE="${TMP_DIR}/deploy";
+  local TMP_FILE="${TMP_DIR}/provisioners;
 
 cat << EOF > "${TMP_FILE}"
   Defaults:%#${SUDO_GID} !requiretty
